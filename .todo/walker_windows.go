54d58507f30dc978b93ce30121b8f8c6f6712ca4
Paepcke, Michael <git@paepcke.de>
2024-05-22 06:04:12 +0200

    auto: sync upstream / update dependencies

---
+++ .todo/walker_windows.go
@@ -0,0 +1,143 @@
+//go:build windows
+
+// walker windows version does not implement (yet) skiping
+// hardlink de-duplicated data (inode seen based on'fileid')
+package codereview
+
+import (
+	"os"
+	"syscall"
+)
+
+const (
+	_modeDir     uint32 = 1 << (32 - 1 - 0)
+	_modeSymlink uint32 = 1 << (32 - 1 - 4)
+)
+
+// fastWalker ...
+func (c *Config) fastWalker() {
+	fi, err := os.Stat(c.Path)
+	if err != nil {
+		errExit("[stat deviceID root dir] [" + c.Path + "] [" + err.Error() + "]")
+	}
+	channel_dir <- c.Path
+	d, ok := fi.Sys().(*syscall.Stat_t)
+	if !ok {
+		errExit("[stat deviceID root dir] [" + c.Path + "]")
+	}
+	for i := 0; i < worker; i++ {
+		go c.walkParse(uint64(d.Dev))
+	}
+}
+
+// walkParse ...
+func (c *Config) walkParse(rootNodeDeviceID uint64) {
+	exclude, skipme := false, false
+	if len(c.Exclude) > 0 {
+		exclude = true
+	}
+	total, processed := 0, 0
+	for path := range channel_dir {
+		list, err := os.ReadDir(path)
+		if err != nil {
+			errOut("unable to read directory [" + path + "] [" + err.Error() + "]")
+			walk.Done()
+			continue
+		}
+		for _, item := range list {
+			total++
+			fi, _ := item.Info()
+			fname := item.Name()
+			if c.SkipHidden {
+				if fname[0] == '.' {
+					continue // skip hidden files if requested
+				}
+			}
+			ftype := uint32(item.Type())
+			name := path + "/" + fname
+			if ftype&_modeSymlink != 0 {
+				continue // skip symlinks
+			}
+			if ftype&_modeDir != 0 {
+				st, _ := fi.Sys().(*syscall.Stat_t)
+				if uint64(st.Dev) != rootNodeDeviceID {
+					continue // skip dirtargets outside fs boundary
+				}
+				if exclude {
+					skipme = false
+					for _, exclude := range c.Exclude {
+						if fname == exclude {
+							skipme = true
+							break // skip exclude list
+						}
+					}
+					if skipme {
+						continue
+					}
+				}
+				walk.Add(1)
+				channel_dir <- name
+				continue
+			}
+			l := len(fname)
+			if l > 2 {
+				if c.LangC {
+					switch fname[l-2:] {
+					case ".c", ".h":
+						processed++
+						channel_c <- name
+						continue
+					}
+				}
+				if c.LangASM {
+					if fname[l-2:] == ".s" || fname[l-2:] == ".S" {
+						processed++
+						channel_asm <- name
+						continue
+					}
+				}
+			}
+			if l > 3 {
+				if c.LangGO && fname[l-3:] == ".go" {
+					processed++
+					channel_go <- name
+					continue
+				}
+				if c.LangSH && fname[l-3:] == ".sh" {
+					processed++
+					channel_sh <- name
+					continue
+				}
+				if c.LangC {
+					if fname[l-3:] == ".cc" || fname[l-3:] == ".hh" {
+						processed++
+						channel_c <- name
+						continue
+					}
+				}
+			}
+			if l > 4 {
+				if c.LangC && fname[l-4:] == ".cpp" {
+					processed++
+					channel_c <- name
+					continue
+				}
+			}
+			if l > 7 {
+				if c.LangMAKE && fname[:8] == "Makefile" {
+					processed++
+					channel_make <- name
+					continue
+				}
+			}
+			// if c.LangC && shebang(name) == "#!/bin/sh" {
+			//	processed++
+			//	channel_sh <- name
+			//	continue
+			// }
+		}
+		walk.Done()
+	}
+	channel_statFiles <- statFiles{totalFiles: total, processedFiles: processed}
+	walkStat.Done()
+}
